04b9d6558d9df669e16193ba8530bf7b62d9613b
2002rahuls <rahulhs0212@gmail.com>
2023-02-07 14:24:25 +0530

    commit

--- gcd_lcm.cpp
+++ gcd_lcm.cpp
@@ -1,22 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-int a=9;
-int b=4;
-int l,g;
-
-for(int i=1;i<=10;i++)
-{
-    for(int j=1;j<=10;j++){
-if(a*i==b*j){
-    l=i;
-    g=j;
-
-}
+vector<long long> lcmAndGcd(long long A , long long B) {
+      vector<int> ans;
+      long long mul=A*B;
+        while(A>0 && B>0){
+            if(A>B) A=A%B;
+            else B=B%A;
+           
+        }
+        int gcd;
+         if(A==0){
+             gcd=B;
+            
+         }
+            else{
+                gcd=A;
+                 
+            }
+            int  lcm;
+            lcm=mul/gcd;
+      
+      cout<<"   lcm is  "<<lcm<<"   gcd is "<<gcd;
+        
+        
     }
-    
-}
-cout<<g<<" "<<l;
+int main(){
+long a=5;
+long b=10;
+int l,g;
+lcmAndGcd(a,b);
+
 
 return 0;
 }
