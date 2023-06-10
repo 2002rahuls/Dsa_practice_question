b296b75b4da96de27079b61e2099d1c92bf49819
2002rahuls <rahulhs0212@gmail.com>
2023-06-10 19:34:14 +0530

    commit

---
+++ byFREQ.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comp(pair<int,int> a,pair<int,int> b){
+    return b>a;
+}
+
+int main(){
+    cout<<"start"<<endl;
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    unordered_map<int,int> mp;
+    for(int i=0;i<n;i++){
+       mp[arr[i]]++;
+    }
+    vector<pair<int,int>> v;
+    for(auto it: mp){
+        v.push_back(it);
+    }
+
+    // for(auto x: v){
+    //     cout<<x.first<<" "<<x.second<< endl;
+    // }
+    sort(v.begin(),v.end(),comp);
+
+    for(auto i:v){
+        for(int j=0;j<i.second;j++){
+            cout<<i.first<<" ";
+
+        }
+        
+    }
+
+    return 0;
+}
--- stl_new.cpp
+++ stl_new.cpp
@@ -10,7 +10,7 @@ int main(){
         
         cin>>arr[i];
     }
-    map<int,int> mp;
+    unordered_map<int,int> mp;
     for(int i=0;i<n;i++){
         mp[arr[i]]++;
     }
